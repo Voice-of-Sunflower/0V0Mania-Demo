31b682c5619216a567f2726f76f3ba6462084e3e
foxIncode <43337753+thefoxIncode@users.noreply.github.com>
2025-02-23 13:46:27 +0800

    Delete Scripts directory
    update scripts

--- Scripts/UI/EndScreen/button_function.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-
-func _on_back_button_pressed():
-	get_tree().change_scene_to_file("res://Scenes/beatmap_list_ui.tscn")
-
-func _on_retry_button_pressed():
-	get_tree().change_scene_to_file("res://Scenes/beatmap_ingame.tscn")
