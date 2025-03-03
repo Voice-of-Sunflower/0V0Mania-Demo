f3fecc67932f7df3b0d14a724062efbf54fc86e5
foxIncode <43337753+thefoxIncode@users.noreply.github.com>
2025-03-03 20:38:38 +0800

    update scripts

---
+++ Scripts/UI/Buttons/set_select_level.gd
@@ -0,0 +1,17 @@
+extends Node
+
+@export var level_button: Button
+var beatmap_path: String = GlobalVariable.selected_beatmap_path
+var selected_level: String
+
+## 获取选择关卡名称与文件夹路径
+func _on_level_button_pressed():
+	selected_level = level_button.text
+	GlobalVariable.selected_level_name = level_button.text
+	GlobalVariable.selected_level_path = beatmap_path.path_join(selected_level)
+	GlobalVariable.selected_beatmap.set_play_button_visible()
+
+func _on_level_button_gui_input(event):
+	if event is InputEventMouse:
+		if event.button_mask == MOUSE_BUTTON_RIGHT:
+			GlobalVariable.show_delete_menu.emit()
---
+++ Scripts/global_score.gd
@@ -0,0 +1,5 @@
+extends Node
+
+var in_game_score: int
+var max_combo: int
+var rate_count: Dictionary = {"MARVELOUS": 0, "PERFECT": 0, "GREAT": 0, "GOOD": 0, "MISS": 0}
