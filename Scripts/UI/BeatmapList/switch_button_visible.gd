95089088f8bb2e1a64c8390b4105e90c05bf77b9
foxIncode <43337753+thefoxIncode@users.noreply.github.com>
2025-03-13 23:51:25 +0800

    Delete Scripts directory

--- Scripts/UI/BeatmapList/switch_button_visible.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-@export var info_container: VBoxContainer
-
-func _ready():
-	GlobalVariable.button_visible_change.connect(_on_change_button_visible)
-
-func _on_change_button_visible():
-	info_container.visible = true
